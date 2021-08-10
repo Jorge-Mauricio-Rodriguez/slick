21b1f1a8c239b90a4d52fe1d5d2be0e4a8896932
Jorge-Mauricio-Rodriguez <jorgerodriguez@getmore.mx>
2021-08-10 11:42:06 -0600

    added outerEdgeLimit option

--- slick/slick.js
+++ slick/slick.js
@@ -66,6 +66,7 @@
                 initialSlide: 0,
                 lazyLoad: 'ondemand',
                 mobileFirst: false,
+                outerEdgeLimit: false,
                 pauseOnHover: true,
                 pauseOnFocus: true,
                 pauseOnDotsHover: false,
@@ -1188,6 +1189,14 @@
                 }
 
                 targetLeft += (_.$list.width() - targetSlide.outerWidth()) / 2;
+            }  else if (_.options.outerEdgeLimit === true) {
+              var lastSlide,
+                  lastLeft,
+                  outerEdgeLimit;
+              lastSlide = _.$slides.last();
+              lastLeft = lastSlide[0] ? lastSlide[0].offsetLeft * -1 : 0;
+              outerEdgeLimit = lastLeft - lastSlide.width() + this.$slider.width();
+              targetLeft = Math.min(Math.max(targetLeft, outerEdgeLimit), 0);
             }
         }
 
